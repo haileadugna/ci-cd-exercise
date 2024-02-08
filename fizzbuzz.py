0783ece708cae371bceff758f370f74fd5f854eb
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2024-02-08 18:55:59 +0300

    test code added

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -11,7 +11,7 @@ def fizzbuzz(n):
         else:
             print(i)
         
-# write test 
+# write test code
 
 def test_fizzbuzz():
     assert fizzbuzz(15) == [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz"]
